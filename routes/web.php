058efea5d7e8c202b42168db2b3aac9080635d9f
dhanikaasandini <dhanikaasandini@gmail.com>
2021-03-03 22:48:10 +0700

    Submit Laravel-Template Blade

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+Route::get('/data-table', function () {
+    return view('data_table');
+});
+
+
+Route::get('/home', 'AuthController@home');
+Route::get('/data_table', 'AuthController@data-table');
+
+
